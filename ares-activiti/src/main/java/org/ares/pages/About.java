0bf69130d1abe72215bbdac33a39a80de4e48b8f
crazzy0727 <962067010@qq.com>
2014-09-12 16:51:53 +0800

    ares-activiti

---
+++ ares-activiti/src/main/java/org/ares/pages/About.java
@@ -0,0 +1,6 @@
+package org.ares.pages;
+
+public class About
+{
+
+}
---
+++ ares-activiti/src/test/java/org/ares/pages/ProcessEngineTest.java
@@ -0,0 +1,35 @@
+package org.ares.pages;
+
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class ProcessEngineTest extends TestCase {
+	@Test
+	public void test() {
+		ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration.createProcessEngineConfigurationFromResourceDefault();
+		ProcessEngine processEngine  = processEngineConfiguration.buildProcessEngine();
+		
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+		repositoryService.createDeployment().addClasspathResource("bpmn/leave/leave-001.bpmn20.xml");
+		
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("leave-001");
+		
+		TaskService taskService = processEngine.getTaskService();
+		Task task = taskService.createTaskQuery().singleResult();
+		
+		System.out.println(task.getName());
+		taskService.complete(task.getId());
+		
+		System.out.println(task.getName());
+	}
+}
